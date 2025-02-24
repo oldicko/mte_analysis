ba0ee2b9e592ab8813a77ce68f84a8d831e0760e
Charles Dickinson <charles@dickinson.io>
2025-02-24 17:28:12 +0000

    Update Flask application to use port 80 and modify route responses

--- alice/app.py
+++ alice/app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from datetime import datetime
 
-SILLY_STRING  = "Charlie"
-
 app = Flask(__name__)
 
 call_count = 0
@@ -10,13 +8,13 @@ call_count = 0
 def get_current_time_string():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-@app.route('/')
+@app.route('/status')
 def status():
     global call_count
     call_count += 1
     current_time = get_current_time_string()
-    return f"{current_time} - {SILLY_STRING}"
+    return f"{current_time} - Eclypses"
 
 @app.route('/count')
 def count():
-    return f"The '/' route has been called {call_count} times."
+    return f"The route has been called {call_count} times."
--- bob/app.py
+++ bob/app.py
@@ -7,9 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def status():
     ip_addr = os.getenv('IP', '127.0.0.1')  # Use the host's IP address
-    port = os.getenv('PORT', '5000')
+    port = os.getenv('PORT', '80')
+    upstream = os.getenv('UPSTREAM')
+    headers = {
+        'x-mte-outbound-token': 'TokenTokenTokenToken',
+        'x-mte-upstream': upstream
+    }
     try:
-        response = requests.get(f'http://{ip_addr}:{port}')
+        response = requests.get(f'http://{ip_addr}:{port}/status', headers=headers)  # Ensure the correct path is used
         response.raise_for_status()
         data = response.text
     except requests.RequestException as e:
